d58dde5da7f3d2005601a712bf3f802f3b1554e7
rafalk <alchemist3@wp.pl>
2017-11-28 00:38:20 +0100

    Initialize agents

--- src/ConsumerAgent.java
+++ src/ConsumerAgent.java
@@ -1,7 +1,25 @@
 import jade.core.Agent;
+import jade.core.AID;
+import jade.core.behaviours.TickerBehaviour;
 
-public class ConsumerAgent extends Agent{
-    protected void setup(){
-        System.out.println("Consumer agent created");
+import java.util.Random;
+
+
+public class ConsumerAgent extends Agent {
+    private int interval = new Random().nextInt(100) + 50;
+    private boolean active = true;
+    private AID[] producers;
+    private int tokensTaken = 0;
+    public TickerBehaviour tickerBehaviour;
+
+
+    // Agent initialization
+    protected void setup() {
+        // Welcome message
+        System.out.println("Consumer-agent " + getAID().getName() + " is ready.");
     }
+
+
+
+
 }
--- src/ProducerAgent.java
+++ src/ProducerAgent.java
@@ -1,8 +1,16 @@
 import jade.core.Agent;
+import jade.core.AID;
 
-public class ProducerAgent extends Agent{
+public class ProducerAgent extends Agent {
+    private Integer id = 0;
+    private int maxTokens = 150;
 
-    protected void setup(){
-        System.out.println("Producer agent created");
+    private AID[] consumerAgents = {new AID("consumer1", AID.ISLOCALNAME),
+            new AID("consumer2", AID.ISLOCALNAME)};
+
+    // Agent initialization
+    protected void setup() {
+        // Welcome message
+        System.out.println("Producer-agent " + getAID().getName() + " is ready.");
     }
 }
