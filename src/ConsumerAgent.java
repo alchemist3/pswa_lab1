3ed4cad547a227b4db84fbe03de37296de45acca
rafalk <alchemist3@wp.pl>
2017-11-27 21:46:38 +0100

    Initial commit

---
+++ src/ConsumerAgent.java
@@ -0,0 +1,7 @@
+import jade.core.Agent;
+
+public class ConsumerAgent extends Agent{
+    protected void setup(){
+        System.out.println("Consumer agent created");
+    }
+}
---
+++ src/ProducerAgent.java
@@ -0,0 +1,8 @@
+import jade.core.Agent;
+
+public class ProducerAgent extends Agent{
+
+    protected void setup(){
+        System.out.println("Producer agent created");
+    }
+}
